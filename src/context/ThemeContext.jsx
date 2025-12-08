16ba57e52922c0bae50e46d29bced48b8c6d7b06
Upendhar <upendharnm.dev@gmail.com>
2025-12-08 15:12:46 +0530

    v1 | default theme set to dark

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   // Load saved theme from localStorage
   useEffect(() => {
